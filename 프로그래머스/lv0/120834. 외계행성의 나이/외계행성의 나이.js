916698422b6c5bafcb2759f7b992c1f0dd0ebafb
바카로이 <113865640+parkharoi@users.noreply.github.com>
2023-06-02 22:32:26 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,10 +1,11 @@
+
 function solution(age) {
-    let answer = []
-    let english = ['a','b','c','d','e','f','g','h','i','j']
-    let str = age.toString().split("")
-    let num = str.map(Number)
-    for (let i=0; i<num.length; i++) {
-        answer.push(english[num[i]])
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+    var answer = "";
+    age = age.toString();
+
+    for(let i=0; i<age.length; i++){
+        answer += alphabet[age[i]];
     }
-    return answer.join('')
-} 
+    return answer;
+}
