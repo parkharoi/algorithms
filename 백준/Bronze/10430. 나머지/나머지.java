995194ca482ca6b0baa596b45067d377a6a20580
바카로이 <113865640+parkharoi@users.noreply.github.com>
2024-03-30 08:24:54 +0900

    [Bronze V] Title: 나머지, Time: 220 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int c = scn.nextInt();
+
+        System.out.printf("%d\n%d\n%d\n%d", (a + b) % c, ((a % c) + (b % c)) % c, (a * b) % c, ((a % c) * (b % c)) % c);
+
+
+    }
+}
