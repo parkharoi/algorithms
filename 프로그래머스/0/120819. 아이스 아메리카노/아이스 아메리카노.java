2d22522a6e9c099c07c826e2aeb3f84f6eb84b8a
바카로이 <113865640+parkharoi@users.noreply.github.com>
2024-08-29 22:01:39 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.04 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,18 +1,9 @@
 class Solution {
     public int[] solution(int money) {
         int[] answer = new int[2];
-        boolean handle = true;
-        int count = 0;
-        while (handle) {
-            if (money < 5500) {
-                handle = false;
-            } else {
-                money -= 5500;
-                count += 1;
-            }
-        }
-        answer[0] = count;
-        answer[1] = money;
+        
+        answer[0] = money / 5500;
+        answer[1] = money % 5500;
         return answer;
     }
 }
