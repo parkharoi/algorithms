6f7ef584fc3878c3d457bdbb36f69430bab555c7
바카로이 <113865640+parkharoi@users.noreply.github.com>
2024-08-29 21:59:23 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.05 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -1,6 +1,5 @@
 class Solution {
     public String solution(String my_string, String letter) {
-        String answer = my_string.replace(letter, "");
-        return answer;
+        return my_string.replace(letter, "");
     }
 }
