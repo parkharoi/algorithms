abdfd57b660302b9087f50e932b0c145641f7c3c
바카로이 <113865640+parkharoi@users.noreply.github.com>
2024-09-04 23:01:51 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.java
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(String message) {
-        int answer = 0;
-        for(int i = 1; i <= message.length(); i++) {
-            answer +=2;
-    }
-        return answer;
+        return message.length()*2;
     }
 }
