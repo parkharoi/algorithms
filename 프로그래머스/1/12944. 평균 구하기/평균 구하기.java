eee9b1f569d59a0d68e98d0df18f073b4d00da6a
바카로이 <113865640+parkharoi@users.noreply.github.com>
2024-09-24 00:58:13 +0900

    [level 1] Title: 평균 구하기, Time: 3.19 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,9 +1,14 @@
+
+import java.util.*;
+import java.lang.*;
+
 class Solution {
     public double solution(int[] arr) {
-        double answer = 0;
-        for(int a : arr) {
-            answer += a;
-        }
-        return answer / arr.length;
+        // double answer = 0;
+        // for(int a : arr) {
+        //     answer += a;
+        // }
+        // return answer / arr.length;
+        return Arrays.stream(arr).average().getAsDouble();
     }
 }
