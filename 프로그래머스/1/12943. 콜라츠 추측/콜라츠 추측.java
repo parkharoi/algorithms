d925f2f0b36eebfb04535bd7b8521e27aa03f87a
바카로이 <113865640+parkharoi@users.noreply.github.com>
2024-10-31 13:05:25 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,23 +1,18 @@
+
 class Solution {
-    public int solution(int num) {
-        int cnt = 0;
-            while(true){
-                if (num == 1){
-                    break;
-                }
-                else if (cnt >= 400) {
-                    cnt = -1;
-                    break;
-                }
-                else if (num % 2 == 0){
-                    num = num / 2;
-                    cnt++;
-                }
-                else {
-                    num = num * 3 + 1;
-                    cnt++;
-                }
+    public int solution(long num) {
+        int answer = 0;
+
+        while(num != 1){
+            answer++;
+            if(answer==500)
+                return -1;
+            if(num%2==0){
+                num/=2;
+            }else{
+                num=num*3+1;
             }
-        return cnt;
+        }
+        return answer;
     }
 }
