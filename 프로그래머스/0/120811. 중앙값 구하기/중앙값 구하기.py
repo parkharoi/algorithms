d50d828f31e0b890484c9d5570d42ccce37342bc
바카로이 <113865640+parkharoi@users.noreply.github.com>
2025-06-28 19:39:38 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.04 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -1,7 +1,3 @@
 def solution(array):
     array.sort()
-    answer = array[len(array) // 2]
-    if answer == -1 :
-        answer = 0 
-           
-    return answer
+    return array[len(array) // 2]
