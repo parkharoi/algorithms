ab52b7c8f3c6685e997d6c66a13f70631419577b
바카로이 <113865640+parkharoi@users.noreply.github.com>
2025-12-24 22:24:36 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,5 +1,15 @@
 class Solution {
     public int solution(int hp) {
-        return (hp / 5) + (hp % 5 / 3) + (hp % 5 % 3);
+        int answer = 0;
+        
+        answer += hp /5;
+        hp%=5;
+        
+        answer +=hp/3;
+        hp%=3;
+        
+        answer +=hp;
+        
+        return answer;
     }
 }
